312fe000d8e0ce8dc6cbe371ae386526adddaa9a
Robert Sesek <rsesek@bluestatic.org>
2010-08-17 00:58:10 -0400

    Add skeleton project.

---
+++ src/main.go
@@ -0,0 +1,13 @@
+
+package main
+
+import (
+  "./server"
+  "fmt"
+)
+
+func main() {
+  fmt.Print("Hello world")
+  server.RunFrontEnd()
+}
+
---
+++ src/server.go
@@ -0,0 +1,10 @@
+package server
+
+import (
+  "fmt"
+)
+
+func RunFrontEnd() {
+  fmt.Print("hi")
+}
+
