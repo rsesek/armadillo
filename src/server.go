e3e3931d9e9dcffc7204b59b427288edca7084f0
Robert Sesek <rsesek@bluestatic.org>
2010-08-18 00:43:55 -0400

    Got the HanldeFunc() working, but not the file server.

--- src/server.go
+++ src/server.go
@@ -2,9 +2,22 @@ package server
 
 import (
   "fmt"
+  "http"
+  "io"
 )
 
-func RunFrontEnd() {
-  fmt.Print("hi")
+const kFrontEndFiles = "/Users/rsesek/Projects/armadillo/out/fe/"
+
+func testHandler(connection *http.Conn, request *http.Request) {
+  fmt.Print("Got a request");
+  io.WriteString(connection, "Hello world")
 }
 
+func RunFrontEnd() {
+  mux := http.NewServeMux()
+  mux.Handle("/fe", http.FileServer(kFrontEndFiles, ""))
+  mux.HandleFunc("/test", testHandler)
+  // mux.Handle()
+  error := http.ListenAndServe(":8084", mux)
+  fmt.Printf("error %v", error)
+}
