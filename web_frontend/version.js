3ae69ac829ea1b2f2f1d2485f3c38ffb02b522db
Armadillo Build Script <armadillo@bluestatic.org>
2010-10-09 17:47:00 -0400

    Stamp version.js @ 271.

--- web_frontend/version.js
+++ web_frontend/version.js
@@ -11,5 +11,5 @@ goog.provide('armadillo.Version');
 
 armadillo.Version.MAJOR = 0;
 armadillo.Version.MINOR = 6;
-armadillo.Version.BUILD = 270;
-armadillo.Version.STAMP = 1286660769;
+armadillo.Version.BUILD = 271;
+armadillo.Version.STAMP = 1286660820;
