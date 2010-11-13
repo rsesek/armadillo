977d68a83a5bf7884c4d5540701f05737305ab0d
Armadillo Build Script <armadillo@bluestatic.org>
2010-11-13 16:54:00 -0500

    Stamp version.js @ 367.

--- web_frontend/version.js
+++ web_frontend/version.js
@@ -11,5 +11,5 @@ goog.provide('armadillo.Version');
 
 armadillo.Version.MAJOR = 0;
 armadillo.Version.MINOR = 6;
-armadillo.Version.BUILD = 346;
-armadillo.Version.STAMP = 1289679756;
+armadillo.Version.BUILD = 367;
+armadillo.Version.STAMP = 1289685240;
