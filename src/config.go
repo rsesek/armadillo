cf3f71366faa8511f0da292d74ba164129d0db2b
Robert Sesek <rsesek@bluestatic.org>
2011-12-28 14:50:11 -0500

    Remove the Users map now that it is taken care of by MFE

--- src/config.go
+++ src/config.go
@@ -25,10 +25,6 @@ type Configuration struct {
 	// forward GET requests and return the result.
 	ProxyURLs []string
 
-	// A map of usernames to MD5-encoded passwords that will be allowed to log in
-	// via a .htaccess style realm.
-	Users map[string]string
-
 	// Whether to include dotfiles (files that begin with a '.'). Users will still
 	// be able to access directories that begin with a '.', but they will not be
 	// included in the list.
