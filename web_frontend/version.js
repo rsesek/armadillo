a33e240364a6b40e53218bd76fbec499707d9073
Armadillo Build Script <armadillo@bluestatic.org>
2011-12-28 17:59:53 -0500

    Stamp version.js @ 588.0.

--- web_frontend/version.js
+++ web_frontend/version.js
@@ -1,7 +1,7 @@
 //
 // Armadillo File Manager
 // Copyright (c) 2011, Robert Sesek <http://www.bluestatic.org>
-// 
+//
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
 // Foundation, either version 3 of the License, or any later version.
@@ -10,6 +10,6 @@
 $.namespace('armadillo.Version');
 
 armadillo.Version.MAJOR = 0;
-armadillo.Version.MINOR = 7;
-armadillo.Version.BUILD = 581.0;
-armadillo.Version.STAMP = 1325111317;
+armadillo.Version.MINOR = 8;
+armadillo.Version.BUILD = 588.0;
+armadillo.Version.STAMP = 1325113193;
