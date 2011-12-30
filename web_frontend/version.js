ccdfb2c6a9630977811ae5006c23202c3da9ebcf
Armadillo Build Script <armadillo@bluestatic.org>
2011-12-30 17:13:47 -0500

    Stamp version.js @ 600.0.

--- web_frontend/version.js
+++ web_frontend/version.js
@@ -11,5 +11,5 @@ $.namespace('armadillo.Version');
 
 armadillo.Version.MAJOR = 0;
 armadillo.Version.MINOR = 8;
-armadillo.Version.BUILD = 599.0;
-armadillo.Version.STAMP = 1325268423;
+armadillo.Version.BUILD = 600.0;
+armadillo.Version.STAMP = 1325283227;
